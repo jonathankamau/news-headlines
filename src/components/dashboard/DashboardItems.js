41b02481e8f4d9ed203e6df4ee2fc73dfeff5644
jonathankamau <jonathan.kamau@andela.com>
2017-11-29 11:48:28 +0300

    feat(dashboard-page): format news sources view to make it more presentable
    Currently the news sources on the news source page are presented as a
    list which is not viewer friendly.
    
    This ensures that the news sources presented in block form on the
    news sources view.
    
    [Delivers #153241414]

--- src/components/dashboard/DashboardItems.js
+++ src/components/dashboard/DashboardItems.js
@@ -4,11 +4,16 @@ import { PropTypes } from 'prop-types';
 const DashboardItems = ({sources}) => {
   console.log(sources.sources);
   return (
-     <div>
+     <div className='news-sources-list-wrapper'>
         {sources.sources.map(source =>
-          <div className='news-list' key={ source.id }>
-            <li>{ source.name }</li>
-            {source.description}
+          <div className='news-sources-list' key={ source.id }>
+            <div><img className='news-source-logo' 
+            src={'https://icons.better-idea.org/icon?url='+source.url+'&amp;size=70..120..200'}></img></div>
+            <div className='news-sources-text'><h2>{ source.name }</h2>
+            <p>{ source.description }</p>
+            <h3><p>Category: { source.category }</p></h3>
+            <a href={ source.url } target='_blank'>{ source.url }</a>
+            </div>
             </div>
         )}
     </div>
